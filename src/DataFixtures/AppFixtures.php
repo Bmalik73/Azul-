8a433adddd08fa4c14655585e4a7381295e783cc
Bmalik73 <bmalik01@live.fr>
2023-01-25 15:35:05 +0100

    update fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use DateTime;
+use App\Entity\MicroPost;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +11,23 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $microPost1 = 
-        // $manager->persist($product);
+        $microPost1 = new MicroPost();
+        $microPost1->setTitle('Bienvenue en France!');
+        $microPost1->setText('Bienvenue en France!');
+        $microPost1->setCreated(new DateTime());
+        $manager->persist($microPost1);
+
+        $microPost2 = new MicroPost();
+        $microPost2->setTitle('Bienvenue au Maroc!');
+        $microPost2->setText('Bienvenue au Maroc!');
+        $microPost2->setCreated(new DateTime());
+        $manager->persist($microPost2);
+
+        $microPost3 = new MicroPost();
+        $microPost3->setTitle('Bienvenue en Espagn!');
+        $microPost3->setText('Bienvenue en Espagne!');
+        $microPost3->setCreated(new DateTime());
+        $manager->persist($microPost3);
 
         $manager->flush();
     }
