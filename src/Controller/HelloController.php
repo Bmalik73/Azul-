0addf0af076c6954081c5a26f545055b1450e597
Bmalik73 <bmalik01@live.fr>
2023-01-26 09:57:16 +0100

    update hello controller

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\MicroPostRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HelloController extends AbstractController
+{
+    private array $messages = [
+        "Hello", "Hi", "Bye"
+    ];
+
+    #[Route('/{limit<\d+>?3}', name: 'app_index')]
+    public function index(int $limit): Response
+    {
+        return new Response(
+            implode(',', array_slice($this->messages, 0, $limit))
+        );
+    }
+
+    #[Route('/messages/{id<\d+>}', name: 'app_show_one')]
+    public function showOne(int $id): Response
+    {
+        return $this->render(
+            'hello/show_one.html.twig',
+            [
+                'message' => $this->messages[$id]
+            ]
+        );
+       // return new Response($this->messages[$id]);
+    }
+}
+
